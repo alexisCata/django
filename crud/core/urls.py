12211ed7d1c6ef5b9824318e5092a3fe5786662e
alexis <a.flores@cathedralsw.com>
2018-05-03 15:00:04 +0200

    changes

--- crud/core/urls.py
+++ crud/core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import list_users, add_user, update_user, delete_user
+from .views import list_users, add_user, add_bank_account, update_user, delete_user
 
 
 urlpatterns = [
     path('', list_users, name='home'),
     path('add', add_user, name='add_user'),
+    path('user/<int:id>/add_bank_account', add_bank_account, name='add_bank_account'),
     path('update/<int:id>', update_user, name='update_user'),
     path('delete/<int:id>', delete_user, name='delete_user'),
 ]
--- crud/core/views.py
+++ crud/core/views.py
@@ -13,13 +13,40 @@ def list_users(request):
     return render(request, 'core/home.html', {'users': users})
 
 
+# @login_required
+# def add_user(request):
+#     form = BankUserForm(request.POST or None)
+#
+#     AccountFormSet = inlineformset_factory(BankUser, BankAccount, form=BankAccountForm, fields=('IBAN',), fk_name='bank_user')
+#     bankuser = BankUser()
+#     formset = AccountFormSet(request.POST or None, instance=bankuser, prefix='form')
+#
+#     if form.is_valid():
+#         prev = form
+#         form = form.save(commit=False)
+#         form.owner = request.user
+#         form.save()
+#
+#         bankuser = BankUser.objects.get(id=form.id)
+#
+#         formset = AccountFormSet(request.POST, instance=bankuser, prefix='form')
+#         if formset.is_valid():
+#             formset.save()
+#             return redirect('home')
+#         else:
+#             bankuser.delete()
+#             form = prev
+#             # formset._errors = formset.non_form_errors()
+#
+#     return render(request, 'core/add.html', {'form': form, 'formset': formset})
+
 @login_required
 def add_user(request):
     form = BankUserForm(request.POST or None)
 
-    AccountFormSet = inlineformset_factory(BankUser, BankAccount, form=BankAccountForm, fields=('IBAN',), fk_name='bank_user')
-    bankuser = BankUser()
-    formset = AccountFormSet(request.POST or None, instance=bankuser, prefix='form')
+    # AccountFormSet = inlineformset_factory(BankUser, BankAccount, form=BankAccountForm, fields=('IBAN',), fk_name='bank_user')
+    # bankuser = BankUser()
+    # formset = AccountFormSet(request.POST or None, instance=bankuser, prefix='form')
 
     if form.is_valid():
         prev = form
@@ -29,16 +56,46 @@ def add_user(request):
 
         bankuser = BankUser.objects.get(id=form.id)
 
-        formset = AccountFormSet(request.POST, instance=bankuser, prefix='form')
-        if formset.is_valid():
-            formset.save()
+        if 'save' in request.POST:
             return redirect('home')
-        else:
-            bankuser.delete()
-            form = prev
+        elif 'save_and_add_ba' in request.POST:
+            return redirect('add_bank_account', bankuser.id)
+
+        # formset = AccountFormSet(request.POST, instance=bankuser, prefix='form')
+        # if formset.is_valid():
+        #     formset.save()
+        #     return redirect('home')
+        # else:
+        #     bankuser.delete()
+        #     form = prev
             # formset._errors = formset.non_form_errors()
 
-    return render(request, 'core/add.html', {'form': form, 'formset': formset})
+    return render(request, 'core/add.html', {'form': form})#, 'formset': formset})
+
+
+@login_required
+def add_bank_account(request, id):
+    form = BankAccountForm(request.POST or None)
+
+    if form.is_valid():
+        form = form.save(commit=False)
+
+        bankuser = BankUser.objects.get(id=id)
+        form.bank_user = bankuser
+        form.save()
+
+        return redirect('update_user', bankuser.id)
+
+        # formset = AccountFormSet(request.POST, instance=bankuser, prefix='form')
+        # if formset.is_valid():
+        #     formset.save()
+        #     return redirect('home')
+        # else:
+        #     bankuser.delete()
+        #     form = prev
+            # formset._errors = formset.non_form_errors()
+
+    return render(request, 'core/add_bank_account.html', {'form': form})#, 'formset': formset})
 
 
 @login_required
@@ -46,16 +103,20 @@ def update_user(request, id):
     user = BankUser.objects.get(id=id)
     form = BankUserForm(request.POST or None, instance=user)
 
-    AccountFormSet = inlineformset_factory(BankUser, BankAccount, fields=('IBAN',))
-    formset = AccountFormSet(request.POST or None, instance=user, prefix='form')
+    accounts= user.bankaccount_set.all()
 
-    if form.is_valid() and formset.is_valid():
+    # AccountFormSet = inlineformset_factory(BankUser, BankAccount, fields=('IBAN',))
+    # formset = AccountFormSet(request.POST or None, instance=user, prefix='form')
+
+    if form.is_valid(): # and formset.is_valid():
         form.save()
-        formset.save()
-        return redirect('home')
+        if 'save' in request.POST:
+            return redirect('home')
+        elif 'save_and_add_ba' in request.POST:
+            return redirect('add_bank_account', user.id)
 
     return render(request, 'core/add.html',
-                  {'form': form, 'user': request.user, 'bank_user': user, 'formset': formset})
+                  {'form': form, 'user': request.user, 'bank_user': user, 'accounts': accounts})#'formset': formset})
 
 
 @login_required
@@ -63,7 +124,10 @@ def delete_user(request, id):
     user = BankUser.objects.get(id=id)
 
     if request.method == 'POST':
-        user.delete()
-        return redirect('home')
+        if "delete" in request.POST:
+            user.delete()
+            return redirect('home')
+        else:
+            return redirect('update_user', id)
 
     return render(request, 'core/delete_confirm.html', {'user_to_delete': user})
